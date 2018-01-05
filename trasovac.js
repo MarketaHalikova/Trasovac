59897e6e9263740460febcbb56b40857ae6d178a
MarketaHalikova <halikova.marketa@seznam.cz>
2018-01-05 18:17:07 +0100

    Rozdělení kódu do modulů

--- trasovac.js
+++ trasovac.js
@@ -6,11 +6,80 @@
 var MapController = (function () {
 
 
+    var MapConstants = {
+        DEFAULT_LATITUDE: 50.084218,
+        DEFAULT_LONGITUDE: 14.441196,
+        DEFAULT_ZOOM: 15,
+        DEFAULT_MAP_TYPES: 'terrain',
+
+    }
+
+    var map = null;
+    var currentLatitude = MapConstants.DEFAULT_LATITUDE;
+    var currentLongitude = MapConstants.DEFAULT_LONGITUDE;
+
+
+    /*
+vycentrovat mapu
+*/
+    var centerMap = function () {
+        map.setCenter(new google.maps.LatLng(parseFloat(currentLatitude), parseFloat(currentLongitude)));
+    };
+    // zjistit aktuální pozici
+    // nepůjde pokud nebudu na https
+    var findPosition = function () {
+        // get current location
+        // check if  Geolocation API is available
+        if (!navigator.geolocation) {
+            console.log('Geolocation API not available');
+            // Geolocation API not available - use default
+            currentLatitude = MapConstants.DEFAULT_LATITUDE;
+            currentLongitude = MapConstants.DEFAULT_LONGITUDE;
+            centerMap();
+        } else {
+            console.log('Geolocation API is available');
+            // asynchronous
+            navigator.geolocation.getCurrentPosition(function (position) {
+                    // Get the coordinates of the current possition.
+                    currentLatitude = position.coords.latitude;
+                    currentLongitude = position.coords.longitude;
+                    centerMap();
+                },
+                // Optional error callback
+                function (error) {
+                    alert('Position is unknown. It will be used a default one!')
+                    currentLatitude = MapConstants.DEFAULT_LATITUDE;
+                    currentLongitude = MapConstants.DEFAULT_LONGITUDE;
+                    centerMap();
+                }
+            );
+        }
+    };
+
     /*
     --------------------- Return part ---------------------
     */
     return {
 
+        init: function (google) {
+
+            var mapElement, mapOptions
+
+            findPosition();
+
+            mapElement = document.getElementById('map');
+            mapOptions = {
+                center: {
+                    lat: MapConstants.DEFAULT_LATITUDE,
+                    lng: MapConstants.DEFAULT_LONGITUDE,
+                },
+                zoom: MapConstants.DEFAULT_ZOOM,
+                mapTypeId: MapConstants.DEFAULT_MAP_TYPES,
+            };
+            map = new google.maps.Map(mapElement, mapOptions);
+
+            centerMap();
+        }
     }
 
 })();
@@ -57,81 +126,24 @@ var DataController = (function () {
   ---------------------- MainController ------------------
   --------------------------------------------------------
 */
-var MainController = (function () {
+var MainController = (function (mapCtrl, uICtrl, dataCtrl) {
 
 
     /*
     --------------------- Return part ---------------------
     */
     return {
+        init: function (google) {
+            mapCtrl.init(google);
+
+        }
 
     }
 
-})();
-
-
-
-var MapConstants = {
-    DEFAULT_LATITUDE: 50.084218,
-    DEFAULT_LONGITUDE: 14.441196,
-    DEFAULT_ZOOM: 15,
-    DEFAULT_MAP_TYPES: 'terrain',
-
-}
-
-var map = null;
-var currentLatitude = MapConstants.DEFAULT_LATITUDE;
-var currentLongitude = MapConstants.DEFAULT_LONGITUDE;
+})(MapController, UIController, DataController);
 
 
 
 var initApp = function () {
-    findPosition();
-    var mapElement = document.getElementById('map');
-    var mapOptions = {
-        center: {
-            lat: MapConstants.DEFAULT_LATITUDE,
-            lng: MapConstants.DEFAULT_LONGITUDE,
-        },
-        zoom: MapConstants.DEFAULT_ZOOM,
-        mapTypeId: MapConstants.DEFAULT_MAP_TYPES,
-    };
-    map = new google.maps.Map(mapElement, mapOptions);
-    centerMap();
-};
-/*
-vycentrovat mapu
-*/
-var centerMap = function () {
-    map.setCenter(new google.maps.LatLng(parseFloat(currentLatitude), parseFloat(currentLongitude)));
-};
-// zjistit aktuální pozici
-// nepůjde pokud nebudu na https
-var findPosition = function () {
-    // get current location
-    // check if  Geolocation API is available
-    if (!navigator.geolocation) {
-        console.log('Geolocation API not available');
-        // Geolocation API not available - use default
-        currentLatitude = MapConstants.DEFAULT_LATITUDE;
-        currentLongitude = MapConstants.DEFAULT_LONGITUDE;
-        centerMap();
-    } else {
-        console.log('Geolocation API is available');
-        // asynchronous
-        navigator.geolocation.getCurrentPosition(function (position) {
-                // Get the coordinates of the current possition.
-                currentLatitude = position.coords.latitude;
-                currentLongitude = position.coords.longitude;
-                centerMap();
-            },
-            // Optional error callback
-            function (error) {
-                alert('Position is unknown. It will be used a default one!')
-                currentLatitude = MapConstants.DEFAULT_LATITUDE;
-                currentLongitude = MapConstants.DEFAULT_LONGITUDE;
-                centerMap();
-            }
-        );
-    }
+    MainController.init(google);
 };
